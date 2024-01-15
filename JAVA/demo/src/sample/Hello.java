b3d95e292648d0b873a01cf9a1a8f275af233ac6
Leejinwoo84 <wlsnwosn@gmail.com>
2024-01-15 17:26:28 +0900

    3.working

---
+++ JAVA/demo/src/sample/Hello.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println();					// syso + Ctrl\space
+		System.out.println("안녕하세요?");
+		
+	}
+
+}
---
+++ webproject/test.java
@@ -0,0 +1,6 @@
+public class MainPlay {
+
+	public static void main(String[] args) {
+        system.out.println("abc");
+    }
+}
